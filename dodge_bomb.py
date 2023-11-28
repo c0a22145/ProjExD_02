77e70320cdd2e0487f16d685794cc92cb41c00f6
fushimity@edu.teu.ac.jp <c0a22145bf@edu.teu.ac.jp>
2023-11-28 15:27:20 +0900

    練習問題完成

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -5,6 +5,28 @@ import pygame as pg
 
 WIDTH, HEIGHT = 1600, 900
 
+delta = {
+    pg.K_UP: (0, -5),  # キー：移動量／値：（横方向移動量，縦方向移動量）
+    pg.K_DOWN: (0, +5),
+    pg.K_LEFT: (-5, 0),
+    pg.K_RIGHT: (+5, 0)
+}
+
+
+def check_bound(rct: pg.Rect) -> tuple[bool, bool]:
+    """
+    オブジェクトが画面内or画面外を判定し、真理値タプルを返す関数
+    引数　rct：こうかとんor爆弾surfaceのRect
+    戻り値：横方向、縦方向にはみ出し判定（画面内：True, 画面外：False）
+    """
+
+    yoko, tate = True, True
+    if rct.left < 0 or WIDTH < rct.right:
+        yoko = False
+    if rct.top < 0 or HEIGHT < rct.bottom:
+        tate = False
+    return yoko, tate
+
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
@@ -12,6 +34,8 @@ def main():
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
+    kk_rct = kk_img.get_rect()  # 練習３：こうかとんSurfaceのRectを抽出する
+    kk_rct.center = 900, 400  # 練習３：こうかとんの初期座標
     bb_img = pg.Surface((20, 20))
     bb_img.set_colorkey((0, 0, 0))
     pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)
@@ -19,50 +43,42 @@ def main():
     bb_rct.centerx = random.randint(0, WIDTH)
     bb_rct.centery = random.randint(0, HEIGHT)
     vx, vy = +5, +5
-    
-    def check_bound(rct: pg.Rect) -> tuple[bool, bool]:
-        """
-        オブジェクトが画面内or画面外を判定し、真理値タプルを返す関数
-        引数　rct：こうかとんor爆弾surfaceのRect
-        戻り値：横方向、縦方向にはみ出し判定（画面内：True, 画面外：False）
-        """
-    
-    yoko, tate = True, True
-    if rct.left < 0 or WIDTH < rct.right:
-        yoko = False
-    if rct.top < 0 or HEIGHT < rct.bottom:
-        tate = False
-        return yoko, tate
-    
+
     clock = pg.time.Clock()
     tmr = 0
-    kk_x, kk_y = 900, 400  # キャラクターの初期の位置
-    kk_spd = 50  # 移動の速度
     while True:
         for event in pg.event.get():
-            if event.type == pg.QUIT: 
+            if event.type == pg.QUIT:
                 return
 
-        keys = pg.key.get_pressed()
+            if kk_rct.colliderect(bb_rct):
+                print("Game Over")
+                return
 
-        # 矢印キーで移動する
-        if keys[pg.K_LEFT]:
-            kk_x -= kk_spd
-        if keys[pg.K_RIGHT]:
-            kk_x += kk_spd
-        if keys[pg.K_UP]:
-            kk_y -= kk_spd
-        if keys[pg.K_DOWN]:
-            kk_y += kk_spd
+            key_lst = pg.key.get_pressed()
+            sum_mv = [0, 0]
+            for k, tpl in delta.items():
+                if key_lst[k]:  # キーが押されたら
+                    sum_mv[0] += tpl[0]
+                    sum_mv[1] += tpl[1]
+
 
         screen.blit(bg_img, [0, 0])
-        screen.blit(kk_img, [kk_x, kk_y])
-        bb_rct.centerx += vx
-        bb_rct.centery += vy
+        kk_rct.move_ip(sum_mv[0], sum_mv[1])
+        if check_bound(kk_rct) != (True, True):
+            kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
+        screen.blit(kk_img, kk_rct)  # 練習３：こうかとんを移動させる
+        bb_rct.move_ip(vx, vy)  # 練習２：爆弾を移動させる
+        yoko, tate = check_bound(bb_rct)
+        if not yoko:  # 横方向にはみ出たら
+            vx *= -1
+        if not tate:  # 縦方向にはみ出たら
+            vy *= -1
+        bb_rct.move_ip(vx, vy) 
         screen.blit(bb_img, bb_rct)
         pg.display.update()
         tmr += 1
-        clock.tick(10)
+        clock.tick(50)
 
 
 if __name__ == "__main__":
