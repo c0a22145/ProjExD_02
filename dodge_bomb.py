b2e35e9f226e642eb3baf1fec9551c0d3be6be7d
fushimity@edu.teu.ac.jp <c0a22145bf@edu.teu.ac.jp>
2023-11-28 14:34:04 +0900

    爆弾移動＋こうかとん移動

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -12,17 +12,18 @@ def main():
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
-    bb_img = pg.Surface((20, 20)) # 練習１：透明なsurfaceを作成する
-    bb_img.set_colorkey((0, 0, 0)) # 黒い部分を透明にしている
+    bb_img = pg.Surface((20, 20))
+    bb_img.set_colorkey((0, 0, 0))
     pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)
     bb_rct = bb_img.get_rect()
     bb_rct.centerx = random.randint(0, WIDTH)
     bb_rct.centery = random.randint(0, HEIGHT)
-    
+    vx, vy = +5, +5
+
     clock = pg.time.Clock()
     tmr = 0
     kk_x, kk_y = 900, 400  # キャラクターの初期の位置
-    kk_spd = 5  # 移動の速度
+    kk_spd = 50  # 移動の速度
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
@@ -31,17 +32,19 @@ def main():
         keys = pg.key.get_pressed()
 
         # 矢印キーで移動する
-        if keys[pg.K_LEFT]: #左
+        if keys[pg.K_LEFT]:
             kk_x -= kk_spd
-        if keys[pg.K_RIGHT]: #右
+        if keys[pg.K_RIGHT]:
             kk_x += kk_spd
-        if keys[pg.K_UP]: #上
+        if keys[pg.K_UP]:
             kk_y -= kk_spd
-        if keys[pg.K_DOWN]: #下
+        if keys[pg.K_DOWN]:
             kk_y += kk_spd
 
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, [kk_x, kk_y])
+        bb_rct.centerx += vx
+        bb_rct.centery += vy
         screen.blit(bb_img, bb_rct)
         pg.display.update()
         tmr += 1
